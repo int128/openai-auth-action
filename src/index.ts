2364fc6ade687702f9fa8ac610af319f8c7711c5
Hidetake Iwata <int128@gmail.com>
2026-09-20 16:45:53 +0900

    Initial import

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import * as core from '@actions/core'
-import { getContext, getOctokit } from './github.js'
 import { run } from './run.js'
 
 try {
-  await run(
-    {
-      name: core.getInput('name', { required: true }),
-    },
-    getOctokit(),
-    await getContext(),
-  )
+  await run({
+    audience: core.getInput('audience', { required: true }),
+    identityProviderId: core.getInput('identity-provider-id', { required: true }),
+    serviceAccountId: core.getInput('service-account-id', { required: true }),
+  })
 } catch (e) {
   core.setFailed(e instanceof Error ? e : String(e))
   console.error(e)
